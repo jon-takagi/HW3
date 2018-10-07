1991ff07f7b83eb5bce60f8704c6a25784a28c41
Jon <jontakagi@reed.edu>
2018-10-06 20:09:39 -0700

    finished q2 and q3

---
+++ q2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+bool isMultiple(int, int);
+bool isMultiple(int arg, int num){
+    return arg % num == 0;
+}
+int main() {
+    int num;
+    cin >> num;
+    int arg;
+    int count = 0;
+    for(int i = 0; i < 10; i++) {
+        cin >> arg;
+        if(isMultiple(arg, num)) {
+            count++;
+            cout << arg;
+            cout << " is a multiple of ";
+            cout << num;
+            cout << "\n";
+        }
+    }
+    cout << count;
+
+}
